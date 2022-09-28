4cd3d01b5752d4a6800ab7a1a95d8de4b92a9565
Jobsidney <jobsidney67@gmail.com>
2022-09-28 08:59:41 +0300

    DOne

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,9 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
+  def create
+    movie=Movie.create(params.permit(:title,:year,:length,:director,:description,:poster_url,:category,:discount,:female_director))
+    render json: movie, status: :created
+  end
 
 end
